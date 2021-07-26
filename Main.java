b53bfa828fb28f565997a7bb24d3910d9b61c960
MikhailPozdeev <84531963+MikhailPozdeev@users.noreply.github.com>
2021-07-27 00:23:14 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 800;
+        int amount = 20;
+
+        int totalBonus = price / amount;
+        System.out.println(totalBonus);
+    }
+}
